5b7f016165155f9aea8dcce0db527aec6eddc674
Uriel Martinez <urielmartz05@outlook.com>
2025-12-01 14:01:30 -0600

    Keys action correction

--- POS System/src/systemgui/PosGui.java
+++ POS System/src/systemgui/PosGui.java
@@ -10,6 +10,7 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.util.HashMap;
 
 import javax.swing.*;
